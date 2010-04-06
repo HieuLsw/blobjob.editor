046bf96460bc236f0578e2a080b6c23d8f0128e2
Lucas Caro <lucascaro@gmail.com>
2010-04-05 22:01:16 -0300

    added cocos 0.3.1beta

--- gamelib/main.py
+++ gamelib/main.py
@@ -7,7 +7,25 @@ package.
 '''
 
 import data
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+import pyglet
+from cocos.director import director
+
+from game_editor_scene import GameEditorScene
 
 def main():
-    print "Hello from your game's main()"
-    print data.load('sample.txt').read()
+    print sys.argv
+
+    pyglet.font.add_directory(data.filepath('fonts'))
+    pyglet.resource.path.append(data.filepath('.'))
+    pyglet.resource.reindex()
+
+    director.init(resizable=True, width=800, height=600, audio=None)
+    director.window.("BlobEdit")
+    s = GameEditorScene()
+    director.run( s )
+    
+#    print data.load('sample.txt').read()
--- run_game.py
+++ run_game.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python
 
+import sys
+# preferrence for the included libs
+sys.path.insert(0, 'libs')
+
 from gamelib import main
 main.main()
