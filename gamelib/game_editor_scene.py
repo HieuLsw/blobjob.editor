6048e31f5f0d6ed04898e0ed30d056ef79c4a85e
Lucas Caro <lucascaro@gmail.com>
2010-04-10 01:53:19 -0300

    added scene that loads level1.xml under data/maps

---
+++ gamelib/game_editor_scene.py
@@ -0,0 +1,9 @@
+from cocos.scene import Scene
+
+
+
+class GameEditorScene(Scene):
+    def __init__(self):
+        super(GameEditorScene, self).__init__()
+        print "creating game editor scene"
+        
